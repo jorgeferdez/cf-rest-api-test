8e75d6539f691a244aaaee94c7d2d388e92e708d
jorgeferdez <136271884+jorgeferdez@users.noreply.github.com>
2024-12-22 21:44:39 -0300

    1

---
+++ .wrangler/tmp/bundle-exD3jj/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "C:\\Users\\jf\\Documents\\GitHub\\cf-rest-api-test\\.wrangler\\tmp\\pages-HKSZoE\\bundledWorker-0.09621401617193315.mjs";
+				import * as __MIDDLEWARE_0__ from "C:\\Users\\jf\\Documents\\GitHub\\cf-rest-api-test\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "C:\\Users\\jf\\Documents\\GitHub\\cf-rest-api-test\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+
+				export * from "C:\\Users\\jf\\Documents\\GitHub\\cf-rest-api-test\\.wrangler\\tmp\\pages-HKSZoE\\bundledWorker-0.09621401617193315.mjs";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
---
+++ pages/_worker.js
@@ -0,0 +1,49 @@
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname.startsWith('/api/')) {
+      const url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=' + env.API_KEY;
+
+      const requestData = {
+        contents: [
+          {
+            parts: [
+              {
+                text: 'Escribe una historia sobre una mochila mágica.',
+              },
+            ],
+          },
+        ],
+        generationConfig: {
+          temperature: 1,
+          topK: 40,
+          topP: 0.95,
+          maxOutputTokens: 1000,
+          responseMimeType: 'text/plain'
+        }
+      };
+
+      try {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(requestData),
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log('Generated Content:', JSON.stringify(data, null, 2));
+        return Response.json(data);
+      } catch (error) {
+        console.error('Error generating content:', error.message);
+        return Response.json({ message: error.message }, { status: 500 });
+      }
+    }
+    return env.ASSETS.fetch(request);
+  },
+}
